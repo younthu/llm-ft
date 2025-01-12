cd762dcc753ece7a00f0a463093792f7707a8493
Andrew Yang <evil_eve_live@outlook.com>
2025-01-12 13:42:51 +0800

    print inputs ids.

--- train.py
+++ train.py
@@ -72,15 +72,17 @@ def train():
         attention_mask = instruction["attention_mask"] + response["attention_mask"] + [1]  # 因为eos token咱们也是要关注的所以 补充为1
         labels = [-100] * len(instruction["input_ids"]) + response["input_ids"] + [tokenizer.pad_token_id]
         if len(input_ids) > MAX_LENGTH:  # 做一个截断
+            print("the input_ids is too long, need to truncate")
             input_ids = input_ids[:MAX_LENGTH]
             attention_mask = attention_mask[:MAX_LENGTH]
             labels = labels[:MAX_LENGTH]
-        return {
+        res =  {
             "input_ids": input_ids,
             "attention_mask": attention_mask,
             "labels": labels
         }
-
+        print(f'res: {res}')
+        return res
     tokenized_id = ds.map(process_func, remove_columns=ds.column_names)
 
     tokenizer.decode(list(filter(lambda x: x != -100, tokenized_id[1]["labels"])))
